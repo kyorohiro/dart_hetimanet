7b5638134b4dd800d4e68c247df8e28de72d70bb
kyorohiro <kyorohiro@gmail.com>
2015-05-10 02:36:38 +0900

    ..

--- lib/hetimanet.dart
+++ lib/hetimanet.dart
@@ -2,3 +2,4 @@ library hetimanet;
 
 export 'src/net/hetisocket.dart';
 export 'src/http/hetihttp.dart';
+export 'src/util/hetiutil.dart';
---
+++ lib/src/util/hetiutil.dart
@@ -0,0 +1,12 @@
+library hetimanet.util;
+
+import 'dart:typed_data' as data;
+import 'dart:math' as math;
+import 'dart:convert' as convert;
+import 'dart:async' as async;
+import 'package:hetimacore/hetimacore.dart';
+import 'package:hetimaregex/hetimaregex.dart' as hregex;
+import '../net/hetisocket.dart';
+import '../tmp/rfctable.dart';
+
+part 'httpurl.dart';
