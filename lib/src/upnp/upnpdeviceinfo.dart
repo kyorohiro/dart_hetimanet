4636364be4de964d7aac1e22eee3c31cb2dec2b4
kyorohiro <kyorohiro@gmail.com>
2015-08-18 14:52:59 +0900

    ..

--- lib/src/upnp/upnpdeviceinfo.dart
+++ lib/src/upnp/upnpdeviceinfo.dart
@@ -1,7 +1,7 @@
 library hetimanet.upnp.deviceinfo;
 
 import 'dart:convert' as convert;
-import 'dart:async' as async;
+import 'dart:async';
 import '../net/hetisocket.dart';
 import '../http/hetihttp.dart';
 import '../util/hetiutil.dart';
@@ -125,68 +125,28 @@ class UpnpDeviceInfo {
     return elements.first.text;
   }
   String _serviceXml = "";
-  async.Future<int> extractService() {
-    async.Completer completer = new async.Completer();
-    //print("----------------------------------------[A]");
-    requestServiceList().then((String serviceXml) {
-      //print("----------------------------------------[B]");
-      //print("serviceXml=" + serviceXml);
-      _serviceXml = serviceXml;
-      _updateServiceXml();
-      completer.complete(0);
-    }).catchError((e) {
-      completer.completeError(e);
-    });
-    return completer.future;
+
+  Future<int> extractService() async {
+    _serviceXml = await requestServiceList();
+    _updateServiceXml();
+    return 0;
   }
 
-  async.Future<String> requestServiceList() {
-    async.Completer<String> completer = new async.Completer();
+  Future<String> requestServiceList() async {
     String location = getValue(UpnpDeviceInfo.KEY_LOCATION, "");
     if (location == "" || location == null) {
-      completer.completeError({});
-      return completer.future;
+      throw {"message":"invalid location"};
     }
 
-    //print("-----requestServiceList()");
     HetiHttpClient client = new HetiHttpClient(socketBuilder, verbose:true);
     HttpUrl url = HttpUrlDecoder.decodeUrl(location);
-    client.connect(url.host, url.port).then((HetiHttpClientConnectResult d) {
-      //print("-----connected[1]");
-      return client.get(url.path);
-    }).then((HetiHttpClientResponse res) {
-      //print("-----get[2] ");
-      //HetiHttpResponseHeaderField field = res.message.find(RfcTable.HEADER_FIELD_CONTENT_LENGTH);
-      return new async.Future.delayed(new Duration(seconds: 1)).then((_) {
-        //
-        //
-        print("-----get[2-0] ${res.body.immutable}");
-        //print("S1 ${res.body.rawcompleterFin.isCompleted}");
-        //return res.body.onFin().then((b) {
-        return res.body.rawcompleterFin.future.then((b) {
-          print("-----get[2-1]");
-          return res.body.getLength().then((int length) {
-            //print("-----get[2-2]");
-            return res.body.getByteFuture(0, length).then((List<int> v) {
-              print("-----get[2-3] ${v.length} ${length} ${client.socket.buffer.size()}");
-              completer.complete(convert.UTF8.decode(v));
-            });
-          });
-        }).catchError((e) {
-          print("-----error[2-3]");
-          throw e;
-        });
-        //print("S2 ${res.body.rawcompleterFin.isCompleted}");
-        // print("S2${res.body.completerFin.isCompleted}");
-        //
-        //
-      });
-    }).catchError((e) {
-      // print("-----error[3]");
-
-      completer.completeError(e);
-    });
-    return completer.future;
+    await client.connect(url.host, url.port);
+    HetiHttpClientResponse res = await client.get(url.path);
+    await new Future.delayed(new Duration(seconds: 1));
+    await res.body.rawcompleterFin.future;
+    int length = await res.body.getLength();
+    List<int> v = await res.body.getByteFuture(0, length);
+    return convert.UTF8.decode(v);
   }
 }
 
