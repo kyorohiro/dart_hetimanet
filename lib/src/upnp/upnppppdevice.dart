18ac70f2a5db55e57b317b8d479763ec00dfd8e6
kyorohiro <kyorohiro@gmail.com>
2015-08-21 17:23:34 +0900

    ..

--- lib/src/upnp/upnppppdevice.dart
+++ lib/src/upnp/upnppppdevice.dart
@@ -6,6 +6,7 @@ import '../http/hetihttp.dart';
 import '../util/hetiutil.dart';
 import 'package:xml/xml.dart' as xml;
 import 'upnpdeviceinfo.dart';
+import 'upnppppdevice_result.dart';
 
 class UpnpPPPDevice {
   static const String KEY_SOAPACTION = "SOAPAction";
@@ -166,72 +167,5 @@ class UpnpPPPDevice {
     if (_verbose == true) {
       print("--${message}");
     }
-  }
-}
-
-class UpnpPPPDeviceRequestResponse {
-  UpnpPPPDeviceRequestResponse(int _resultCode, String _body) {
-    body = _body;
-    resultCode = _resultCode;
-  }
-  String body;
-  int resultCode;
-}
-
-class UpnpGetExternalIPAddressResponse {
-  int resultCode = 200;
-  String externalIp = "";
-  UpnpGetExternalIPAddressResponse(int _resultCode, String _externalIp) {
-    resultCode = _resultCode;
-    externalIp = _externalIp;
-  }
-}
-
-class UpnpAddPortMappingResponse {
-  int resultCode = 200;
-  UpnpAddPortMappingResponse(int _resultCode) {
-    resultCode = _resultCode;
-  }
-}
-
-class UpnpDeletePortMappingResponse {
-  int resultCode = 200;
-  UpnpDeletePortMappingResponse(int _resultCode) {
-    resultCode = _resultCode;
-  }
-}
-
-class UpnpGetGenericPortMappingResponse {
-  static final String KEY_NewRemoteHost = "NewRemoteHost";
-  static final String KEY_NewExternalPort = "NewExternalPort";
-  static final String KEY_NewProtocol = "NewProtocol";
-  static final String KEY_NewInternalPort = "NewInternalPort";
-  static final String KEY_NewInternalClient = "NewInternalClient";
-  static final String KEY_NewEnabled = "NewEnabled";
-  static final String KEY_NewPortMappingDescription = "NewPortMappingDescription";
-  static final String KEY_NewLeaseDuration = "NewLeaseDuration";
-
-  UpnpPPPDeviceRequestResponse _response = null;
-  UpnpGetGenericPortMappingResponse(UpnpPPPDeviceRequestResponse response) {
-    _response = response;
-  }
-
-  int get resultCode => _response.resultCode;
-
-  String getValue(String key, String defaultValue) {
-    if (_response.resultCode != 200) {
-      return defaultValue;
-    }
-    xml.XmlDocument document = xml.parse(_response.body);
-    Iterable<xml.XmlElement> elements = document.findAllElements(key);
-    if (elements == null || elements.length <= 0) {
-      return defaultValue;
-    }
-    return elements.first.text;
-  }
-
-  @override
-  String toString() {
-    return _response.body.toString();
   }
 }
